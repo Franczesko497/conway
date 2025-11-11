2e8564bf91a5f0b7e99f3476d5680d4bf5d9c36d
Franczesko497 <franio.przybyla@gmail.com>
2025-11-11 21:11:41 +0100

    Update conway.py

--- conway.py
+++ conway.py
@@ -1,50 +1,43 @@
 import tkinter as tk
-def on_click(event):
-    """Zmienia kolor klikniętego pola."""
-    label = event.widget  # obiekt, który został kliknięty
-    # Jeśli już jest żółty — przywróć oryginalny
-    if label["bg"] == "gray64":
-        label["bg"] = "gray11"
-    else:
-        label["bg"] = "gray64"
 
-def build_grid(size_x, size_y):
-    for widget in frame.winfo_children():
-        widget.destroy()
-    labels.clear()
-    for r in range(size_x):
-        row = []
-        for c in range(size_y):
-            label = tk.Label( #tworzenie obiektu label
-                frame,
-                bg="gray64",
-                width=5,
-                height=2,
-                relief="solid",
-                borderwidth=0
-            )
-            label.grid(row=r+30, column=c+30, padx=0, pady=0) #umieszczenie obiektu
-            label.bind("<Button-1>", on_click)  # kliknięcie myszą
-            row.append(label)
-        labels.append(row)
+def on_click(event):
+    item = event.widget.find_closest(event.x, event.y)
+    color = event.widget.itemcget(item, "fill")
+    if color == "gray64":
+        new_color = "gray11"
+    else:
+        new_color = "gray64"
+    event.widget.itemconfig(item, fill=new_color)
+
+def build_grid(size):
+    cell_size = 1080/size
+    canvas.delete("all")
+    for r in range(size):
+        for c in range(int(size * (16/9))):
+            x1 = c * cell_size + 110#lewy-górny
+            y1 = r * cell_size #prawy-górny
+            x2 = x1 + cell_size + 110#lewy-dolny
+            y2 = y1 + cell_size #prawy-dolny
+            canvas.create_rectangle(x1, y1, x2, y2, fill="gray64", outline="black")
 
 def update_grid(val):
     size = int(val)
-    build_grid(int(size), int(size*1.5))
+    build_grid(size)
 
-root = tk.Tk() #inicjalizacja
+root = tk.Tk()
 root.geometry("1920x1080")
-root.title("Conway's game of life'") #nazwa
-frame = tk.Frame(root, bg="black") #ramka (inicjalizacja i kolor)
-frame.pack(padx=1, pady=1) # rozmiar ramki
+root.title("Game of Life")
 
+canvas = tk.Canvas(root, bg="light goldenrod")
+canvas.pack(fill="both", expand=True)
+canvas.bind("<Button-1>", on_click)
 
-labels = []
-w = tk.Scale(root, from_=1, to=25, orient="horizontal", label="     Size", command = update_grid)
-w.place(x = 0, y = 0)
-build_grid(10, 15)
+w = tk.Scale(root, from_=10, to=70, orient="horizontal", label="     Size", command = update_grid)
+w.place(x = 3, y = 220)
+build_grid(9)
 w.set(10)
+
+button = tk.Button(root, text="Start", activebackground="blue", activeforeground="white")
+button.place(x = 100, y = 100)
+
 root.mainloop()
-#labels["bg"] = kolor
-#gray11 = zaludniona
-#gray64 = pusta
